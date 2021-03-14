6d7b027236b82097bf09e1a29d33f6fbba3bd502
Mayank Raj <72989762+mayankrj@users.noreply.github.com>
2021-03-14 22:59:41 +0530

    Revert "good"

---
+++ nvmshop/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home' ;
+import About from './components/About' ;
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <About/>
+
+    </div>
+  );
+}
+
+export default App;
